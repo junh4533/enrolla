92e5626cc9e806ebe3fbc8f4cb1526fd1927489b
Jun <junh4533@gmail.com>
2020-11-24 02:51:56 -0500

    finish axios requests for student info page

--- Client/src/Pages/Preferences/Preferences.js
+++ Client/src/Pages/Preferences/Preferences.js
@@ -1,37 +1,161 @@
-import React from "react";
+import React, { Component, useState, useEffect } from "react";
+import Axios from "axios";
+import Cookies from "js-cookie";
 import "./Preferences.scss";
 
 const Preferences = () => {
+  const [courseList, setCourseList] = useState([]);
+  const [coursesToTake, setCoursesToTake] = useState([]);
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/query/course-list")
+      .then((response) => {
+        setCourseList(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="preferences-page">
       {/* ***********START CODING HERE***********  */}
       <h1>Preferences Page</h1>
       <div className="container">
         <div className="form-group">
-          <label>
-            Select all the courses you’d like to take for the semester:
+          <div className="input-group mb-3">
+            <label className="d-block">Courses You Would Like to Take:</label>
+            <br />
+            <select
+              className="custom-select"
+              id="courses"
+              name="courses"
+              multiple
+              onChange={(e) => {
+                const values = [...e.target.selectedOptions].map((opt) =>
+                  String(opt.value)
+                );
+                setCoursesToTake(values);
+                console.log(values);
+              }}
+            >
+              {courseList.map((val) => {
+                return (
+                  <option value={val.Course_Name}>{val.Course_Name}</option>
+                );
+              })}
+            </select>
+          </div>
+        </div>
+        <div className="form-group">
+          <label>How many credits do you have?</label>
+          <input
+            type="number"
+            className="form-control"
+            id="credits"
+            name="credits"
+            // onChange={(e) => {
+            //   setPassword(e.target.value);
+            // }}
+          />
+        </div>
+        <div className="form-check">
+          <label className="form-check-label">
+            <input type="checkbox" className="form-check-input" value="" />
+            Mondays
+            <div className="form-group">
+              <label for="usr">From:</label>
+              <input type="time" name="appt" />
+            </div>
+            <div className="form-group">
+              <label for="usr">To:</label>
+              <input type="time" name="appt" />
+            </div>
           </label>
-          <select id="major" name="major" multiple>
-            <option value="CIS">Computer Information Systems</option>
-            <option value="PAF">Public Affairs</option>
-            <option value="ECO">Economics</option>
-          </select>
         </div>
-        <div className="form-group">
-          <label>Minor:</label>
-          <select id="minor" name="minor" multiple>
-            <option value="ART">Art</option>
-            <option value="THE">Theater</option>
-          </select>
+        <div className="form-check">
+          <label className="form-check-label">
+            <input type="checkbox" className="form-check-input" value="" />
+            Tuesday
+            <div className="form-group">
+              <label for="usr">From:</label>
+              <input type="time" name="appt" />
+            </div>
+            <div className="form-group">
+              <label for="usr">To:</label>
+              <input type="time" name="appt" />
+            </div>
+          </label>
         </div>
-        <div className="form-group">
-          <label>Courses Taken:</label>
-          <select id="courses" name="courses" multiple>
-            <option value="CIS2300">CIS 2300</option>
-            <option value="CIS3100">CIS 3100</option>
-            <option value="CIS3400">CIS 3400</option>
-            <option value="CIS5800">CIS 5800</option>
-          </select>
+        <div className="form-check">
+          <label className="form-check-label">
+            <input type="checkbox" className="form-check-input" value="" />
+            Wednesday
+            <div className="form-group">
+              <label for="usr">From:</label>
+              <input type="time" name="appt" />
+            </div>
+            <div className="form-group">
+              <label for="usr">To:</label>
+              <input type="time" name="appt" />
+            </div>
+          </label>
+        </div>
+        <div className="form-check">
+          <label className="form-check-label">
+            <input type="checkbox" className="form-check-input" value="" />
+            Thursday
+            <div className="form-group">
+              <label for="usr">From:</label>
+              <input type="time" name="appt" />
+            </div>
+            <div className="form-group">
+              <label for="usr">To:</label>
+              <input type="time" name="appt" />
+            </div>
+          </label>
+        </div>
+        <div className="form-check">
+          <label className="form-check-label">
+            <input type="checkbox" className="form-check-input" value="" />
+            Friday
+            <div className="form-group">
+              <label for="usr">From:</label>
+              <input type="time" name="appt" />
+            </div>
+            <div className="form-group">
+              <label for="usr">To:</label>
+              <input type="time" name="appt" />
+            </div>
+          </label>
+        </div>
+        <div className="form-check">
+          <label className="form-check-label">
+            <input type="checkbox" className="form-check-input" value="" />
+            Saturday
+            <div className="form-group">
+              <label for="usr">From:</label>
+              <input type="time" name="appt" />
+            </div>
+            <div className="form-group">
+              <label for="usr">To:</label>
+              <input type="time" name="appt" />
+            </div>
+          </label>
+        </div>
+        <div className="form-check">
+          <label className="form-check-label">
+            <input type="checkbox" className="form-check-input" value="" />
+            Sunday
+            <div className="form-group">
+              <label for="usr">From:</label>
+              <input type="time" name="appt" />
+            </div>
+            <div className="form-group">
+              <label for="usr">To:</label>
+              <input type="time" name="appt" />
+            </div>
+          </label>
         </div>
         <button type="submit" className="btn btn-primary">
           Submit

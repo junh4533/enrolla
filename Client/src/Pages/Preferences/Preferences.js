415f8ee87013cd522e1d3a1cbcc90369833c7b40
Jun <junh4533@gmail.com>
2020-11-25 16:50:25 -0500

    finish axios requests for preferences page

--- Client/src/Pages/Preferences/Preferences.js
+++ Client/src/Pages/Preferences/Preferences.js
@@ -5,6 +5,23 @@ import "./Preferences.scss";
 const Preferences = () => {
   const [courseList, setCourseList] = useState([]);
   const [coursesToTake, setCoursesToTake] = useState([]);
+  const [credits, setCredits] = useState("");
+  // const [days, setDays] = useState([]);
+  // const [hours, setHours] = useState({
+  //   start: "",
+  //   end: ""
+  // });
+
+  const [monday, setMonday] = useState({});
+  const [tuesday, setTuesday] = useState({});
+  const [wednesday, setWednesday] = useState({});
+  const [thursday, setThursday] = useState({});
+  const [friday, setFriday] = useState({});
+  const [saturday, setSaturday] = useState({});
+  const [sunday, setSunday] = useState({});
+
+  const username = Cookies.get("user");
+
   useEffect(() => {
     Axios.get("http://localhost:3001/api/query/course-list")
       .then((response) => {
@@ -15,6 +32,48 @@ const Preferences = () => {
         console.log(error);
       });
   }, []);
+  const handleCheckbox = (element) => {
+    element.onchange = function () {
+      if (element.checked) {
+        element.nextElementSibling.getElementsByTagName(
+          "input"
+        )[0].disabled = false;
+        element.nextElementSibling.nextElementSibling.getElementsByTagName(
+          "input"
+        )[0].disabled = false;
+      } else {
+        element.nextElementSibling.getElementsByTagName(
+          "input"
+        )[0].disabled = true;
+        element.nextElementSibling.nextElementSibling.getElementsByTagName(
+          "input"
+        )[0].disabled = true;
+      }
+    };
+  };
+
+  const PreferencesQuery = () => {
+    Axios.post("http://localhost:3001/api/query/preferences", {
+      credits: credits,
+      coursesToTake: coursesToTake,
+      monday: monday,
+      tuesday: tuesday,
+      wednesday: wednesday,
+      thursday: thursday,
+      friday: friday,
+      saturday: saturday,
+      sunday: sunday,
+      username: username,
+      // days: days,
+      // hours: hours,
+    })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="preferences-page">
@@ -51,112 +110,198 @@ const Preferences = () => {
           <input
             type="number"
             className="form-control"
-            id="credits"
             name="credits"
-            // onChange={(e) => {
-            //   setPassword(e.target.value);
-            // }}
+            onChange={(e) => {
+              setCredits(e.target.value);
+            }}
           />
         </div>
         <div className="form-check">
           <label className="form-check-label">
-            <input type="checkbox" className="form-check-input" value="" />
+            <input
+              type="checkbox"
+              className="form-check-input"
+              onChange={(e) => {
+                handleCheckbox(e.target);
+              }}
+            />
             Mondays
             <div className="form-group">
-              <label for="usr">From:</label>
-              <input type="time" name="appt" />
+              <label>From:</label>
+              <input
+                type="time"
+                name="monday-start"
+                onBlur={(e) => {
+                  setMonday((prevState) => ({
+                    start: e.target.value,
+                    ...prevState,
+                  }));
+                }}
+                disabled
+              />
             </div>
             <div className="form-group">
-              <label for="usr">To:</label>
-              <input type="time" name="appt" />
+              <label>To:</label>
+              <input
+                type="time"
+                name="monday-end"
+                onBlur={(e) => {
+                  setMonday((prevState) => ({
+                    ...prevState,
+                    end: e.target.value,
+                  }));
+                }}
+                disabled
+              />
             </div>
           </label>
         </div>
+
         <div className="form-check">
           <label className="form-check-label">
-            <input type="checkbox" className="form-check-input" value="" />
+            <input
+              type="checkbox"
+              className="form-check-input"
+              onChange={(e) => {
+                handleCheckbox(e.target);
+              }}
+            />
             Tuesday
             <div className="form-group">
-              <label for="usr">From:</label>
-              <input type="time" name="appt" />
+              <label>From:</label>
+              <input
+                type="time"
+                name="tuesday-start"
+                disabled
+                onBlur={(e) => {
+                  setTuesday((prevState) => ({
+                    start: e.target.value,
+                    ...prevState,
+                  }));
+                }}
+              />
             </div>
             <div className="form-group">
-              <label for="usr">To:</label>
-              <input type="time" name="appt" />
+              <label>To:</label>
+              <input
+                type="time"
+                name="tuesday-end"
+                disabled
+                onBlur={(e) => {
+                  setTuesday((prevState) => ({
+                    end: e.target.value,
+                    ...prevState,
+                  }));
+                }}
+              />
             </div>
           </label>
         </div>
         <div className="form-check">
           <label className="form-check-label">
-            <input type="checkbox" className="form-check-input" value="" />
+            <input
+              type="checkbox"
+              className="form-check-input"
+              onChange={(e) => {
+                handleCheckbox(e.target);
+              }}
+            />
             Wednesday
             <div className="form-group">
-              <label for="usr">From:</label>
-              <input type="time" name="appt" />
+              <label>From:</label>
+              <input type="time" name="wednesday-start" disabled />
             </div>
             <div className="form-group">
-              <label for="usr">To:</label>
-              <input type="time" name="appt" />
+              <label>To:</label>
+              <input type="time" name="wednesday-end" disabled />
             </div>
           </label>
         </div>
         <div className="form-check">
           <label className="form-check-label">
-            <input type="checkbox" className="form-check-input" value="" />
+            <input
+              type="checkbox"
+              className="form-check-input"
+              onChange={(e) => {
+                handleCheckbox(e.target);
+              }}
+            />
             Thursday
             <div className="form-group">
-              <label for="usr">From:</label>
-              <input type="time" name="appt" />
+              <label>From:</label>
+              <input type="time" name="thursday-start" disabled />
             </div>
             <div className="form-group">
-              <label for="usr">To:</label>
-              <input type="time" name="appt" />
+              <label>To:</label>
+              <input type="time" name="thursday-end" disabled />
             </div>
           </label>
         </div>
         <div className="form-check">
           <label className="form-check-label">
-            <input type="checkbox" className="form-check-input" value="" />
+            <input
+              type="checkbox"
+              className="form-check-input"
+              onChange={(e) => {
+                handleCheckbox(e.target);
+              }}
+            />
             Friday
             <div className="form-group">
-              <label for="usr">From:</label>
-              <input type="time" name="appt" />
+              <label>From:</label>
+              <input type="time" name="friday-start" disabled />
             </div>
             <div className="form-group">
-              <label for="usr">To:</label>
-              <input type="time" name="appt" />
+              <label>To:</label>
+              <input type="time" name="friday-end" disabled />
             </div>
           </label>
         </div>
         <div className="form-check">
           <label className="form-check-label">
-            <input type="checkbox" className="form-check-input" value="" />
+            <input
+              type="checkbox"
+              className="form-check-input"
+              onChange={(e) => {
+                handleCheckbox(e.target);
+              }}
+            />
             Saturday
             <div className="form-group">
-              <label for="usr">From:</label>
-              <input type="time" name="appt" />
+              <label>From:</label>
+              <input type="time" name="saturday-start" disabled />
             </div>
             <div className="form-group">
-              <label for="usr">To:</label>
-              <input type="time" name="appt" />
+              <label>To:</label>
+              <input type="time" name="saturday-end" disabled />
             </div>
           </label>
         </div>
         <div className="form-check">
           <label className="form-check-label">
-            <input type="checkbox" className="form-check-input" value="" />
+            <input
+              type="checkbox"
+              className="form-check-input"
+              onChange={(e) => {
+                handleCheckbox(e.target);
+              }}
+            />
             Sunday
             <div className="form-group">
-              <label for="usr">From:</label>
-              <input type="time" name="appt" />
+              <label>From:</label>
+              <input type="time" name="sunday-start" disabled />
             </div>
             <div className="form-group">
-              <label for="usr">To:</label>
-              <input type="time" name="appt" />
+              <label>To:</label>
+              <input type="time" name="sunday-end" disabled />
             </div>
           </label>
         </div>
-        <button type="submit" className="btn btn-primary">
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={PreferencesQuery}
+        >
           Submit
         </button>
       </div>
